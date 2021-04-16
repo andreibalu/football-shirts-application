c7a5b89c5f7203876569df7402b0566c315f4f86
Andrei Baluta <andrei.baluta@yahoo.com>
2021-04-16 16:12:46 +0300

    FSSA-12 Created GUI for shop owner screen

--- src/main/java/org/loose/fis/fssa/controllers/ShopOwnerHomeController.java
+++ src/main/java/org/loose/fis/fssa/controllers/ShopOwnerHomeController.java
@@ -6,12 +6,57 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ShopOwnerHomeController {
 
-	 @FXML
-	 private Button Logout;
+	@FXML
+    private Button Logout;
+	
+	@FXML
+	private Button addButton;
+
+	@FXML
+	private Button editButton;
+
+	@FXML
+	private Button removeButton;
+
+    @FXML
+    private TextField teamField;
+
+    @FXML
+    private TextField leagueField;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private TextField imageField;
+
+    @FXML
+    private TableView<?> tableShirts;
+
+    @FXML
+    private TableColumn<?, ?> col_team;
+
+    @FXML
+    private TableColumn<?, ?> col_league;
+
+    @FXML
+    private TableColumn<?, ?> col_price;
+
+    @FXML
+    private TableColumn<?, ?> col_quantity;
+
+    @FXML
+    private TableColumn<?, ?> col_image;
 	
 	@FXML
     void handleLogout(ActionEvent event) throws Exception {
@@ -21,4 +66,34 @@ public class ShopOwnerHomeController {
 	     primaryStage.setScene(new Scene(root, 400, 300));
 	     primaryStage.show();
     }
+	
+	
+	@FXML
+    void handleAddShirts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEditShirts(ActionEvent event) {
+
+    }
+	
+	
+    @FXML
+    void handleRemoveShirts(ActionEvent event) {
+
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
